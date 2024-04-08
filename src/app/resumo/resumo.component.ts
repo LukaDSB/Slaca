6fb33fa06f6c733764d22aeafd6aed402632e9af
Lucas Benevenute <souzalucas6@hotmail.com>
2024-04-08 17:37:15 -0300

    Criando Accordion Div

--- src/app/resumo/resumo.component.ts
+++ src/app/resumo/resumo.component.ts
@@ -8,12 +8,17 @@ import { repeat } from 'cypress/types/lodash';
 })
 export class ResumoComponent implements OnInit {
 
-  loremIpsum: string = this.gerarLoremIpsum(10);
+  loremIpsum: string = this.gerarLoremIpsum(20);
+  expandido: boolean = false;
 
   gerarLoremIpsum(tamanho: number): string{
     const texto = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
     return texto.repeat(tamanho);
+  }
+
+  expandir() {
+      this.expandido = !this.expandido;
   }
 
   constructor() { }
