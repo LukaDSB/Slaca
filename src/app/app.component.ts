1cce5dea1858968cbbc17db5004a2c5b842c0c63
Lucas <souzalucas6@hotmail.com>
2024-04-10 21:15:03 -0300

    Versão final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,24 @@ export class AppComponent {
   criandoNovoTopico:boolean = false;
   carregandoTopico:boolean = false;
 
+  loremIpsum1: string = this.gerarLoremIpsum(4);
+  loremIpsum2: string = this.gerarLoremIpsum(5);
+  loremIpsum3: string = this.gerarLoremIpsum(10);
+  expandido: boolean = false;
+  resto: boolean = true;
+  alturaPainel = 'auto';
+
+  clickExpandir() {
+    this.resto = !this.resto;
+    this.alturaPainel = this.resto ? 'auto' : '0';
+  }
+  
+  gerarLoremIpsum(tamanho: number): string{
+    const texto = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
+    return texto.repeat(tamanho);
+  }
+
   exibirRespostas(){
     this.exibir = !this.exibir;
   }
